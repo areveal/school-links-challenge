6b05aae125fd06023dd3b54d6841d1189b56b83c
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-22 20:43:42 -0600

    Adds cordsheaders config
    Allows our front end to talk to our backend.

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-wngxcbin@hgt&z)p@b$cwwb)_8yke%m9b6163yj@*s=4^5gw@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# this should be put at the end of the settings.py file
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
