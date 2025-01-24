c4c4398edf1dfc3ef6388d0452d48c6062398745
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-24 11:49:07 -0600

    Adds ExamAttribute code field
    This gives us the flexibility to better handle exam attributes with code's that aren't very human readable.

--- api/exams/models/exam_attribute.py
+++ api/exams/models/exam_attribute.py
@@ -13,6 +13,7 @@ class ExamAttribute(models.Model):
 
     exam = models.ForeignKey('exams.Exam', related_name="attributes", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
     data_type = models.CharField(choices=DataTypeChoices.choices, default=DataTypeChoices.STRING, max_length=20)
 
     def __str__(self):
