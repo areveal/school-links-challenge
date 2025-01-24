6d95483fc29c4918c02bd30950305b33473f3a97
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-24 17:02:34 -0600

    Adds API querying
    Adds necessary sorting, filtering and searching for expected UI to results API view

--- api/exams/views/result.py
+++ api/exams/views/result.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework import filters
+from django_filters import rest_framework
 
 from exams.models import Result
 from exams.serializers import ResultSerializer
@@ -9,6 +11,11 @@ class ResultsListCreateView(generics.ListCreateAPIView):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
     permission_classes = [AllowAny]
+    filter_backends = [filters.OrderingFilter, rest_framework.DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['student__first_name', 'student__last_name']
+    filterset_fields = ['exam__subject', 'exam__name', 'exam__grade', 'exam__state', 'exam__code']
+    ordering_fields = ['score', 'date']
+    ordering = ['-date']
 
 
 class ResultDetailView(generics.RetrieveUpdateDestroyAPIView):
