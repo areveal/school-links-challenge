6a1c86b74a62a4895eb75daded6169e613114eb5
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-24 15:21:15 -0600

    Modifies ResultPropertyListCreateView
    Refactors creating/listing result properties to only look at a single result.
    
    Queryset of properties is list of properties related to a given Result object derived from the path.

--- api/exams/views/result_property.py
+++ api/exams/views/result_property.py
@@ -1,17 +1,38 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from exams.models import ResultProperty
-from exams.serializers import ResultPropertySerializer
+from exams import models
+from exams import serializers
 
 
 class ResultPropertyListCreateView(generics.ListCreateAPIView):
-    queryset = ResultProperty.objects.all()
-    serializer_class = ResultPropertySerializer
+    """
+    This class-based view provides functionality to list all ResultProperty
+    of a specific Result object and to create new properties for it.
+    """
+    serializer_class = serializers.ResultPropertySerializer
     permission_classes = [AllowAny]
+
+    def get_object(self):
+        return models.Result.objects.get(id=self.kwargs.get("pk"))
+
+    def get_queryset(self):
+        """
+        Returns: All ResultProperties for the given Result
+        """
+        return self.get_object().properties.all()
+
+    def get_serializer_context(self):
+        """
+        The method extends the existing serializer context with additional details
+        by including the result of the `get_object()` method.
+        """
+        context = super().get_serializer_context()
+        context["result"] = self.get_object()
+        return context
 
 
 class ResultPropertyDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ResultProperty.objects.all()
-    serializer_class = ResultPropertySerializer
+    queryset = models.ResultProperty.objects.all()
+    serializer_class = serializers.ResultPropertySerializer
     permission_classes = [AllowAny]
