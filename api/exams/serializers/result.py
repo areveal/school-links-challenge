ee578309c191c9a492283db6af843555fa268468
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-24 15:22:30 -0600

    Modifies ResultSerializer
    Makes serializer a better source for displaying relevant information that will be needed in our front end.

--- api/exams/serializers/result.py
+++ api/exams/serializers/result.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
 from exams.models import Result
+from exams.serializers import ExamSerializer
+from students.serializers import StudentSerializer
 
 
 class ResultSerializer(serializers.ModelSerializer):
+    properties = serializers.JSONField()
+
     class Meta:
         model = Result
-        fields = "__all__"
+        fields = ["id", "student", "exam", "date", "properties"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        # Build custom properties dict
+        properties = {}
+        for result_property in instance.properties.all():
+            properties.update({result_property.attribute.name: result_property.value})
+        representation["properties"] = properties
+        # We want repr of student to be the serialized data, but the write should retain the FK expectation.
+        representation["student"] = StudentSerializer(instance.student).data
+        # We want repr of exam to be the serialized data, but the write should retain the FK expectation.
+        representation["exam"] = ExamSerializer(instance.exam).data
+        return representation
