09b58e92b6f1fdc77c0716f7b522443a63cf30dd
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-24 15:19:30 -0600

    Modify ResultPropertySerializer
    Touch up ResultPropertySerializer:
    * Adds data type validation
    * Uses Factory to create new properties
    * modifies representation to allow polymorphic model to get value.
    * Removes "result" as writable option
    * Limits attributes to exam definitions related to Result

--- api/exams/serializers/result_property.py
+++ api/exams/serializers/result_property.py
@@ -1,9 +1,55 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from exams.factories import ResultPropertyFactory
 from exams.models import ResultProperty
 
 
 class ResultPropertySerializer(serializers.ModelSerializer):
+    value = serializers.JSONField(write_only=True)
+
     class Meta:
         model = ResultProperty
-        fields = "__all__"
+        fields = ["id", "result", "attribute", "value"]
+        read_only_fields = ["result"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.result = self.context.get("result")
+        if self.result:
+            # Limit available attributes for selection to ExamAttributes related to the given Result
+            self.fields["attribute"].queryset = self.result.exam.attributes
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["value"] = instance.value
+
+        return representation
+
+    def validate(self, data):
+        """
+        Ensure value being passed matches data type of given attribute
+        """
+        exam_definition = data["attribute"]
+        value = data["value"]
+
+        # Match the value against the data type of the exam definition
+        expected_type = exam_definition.data_type
+        if not isinstance(value, eval(expected_type)):
+            raise ValidationError({"value": f"Must be of type {expected_type}"})
+
+        return data
+
+    def create(self, validated_data):
+        """
+        Create ResultProperty using ResultProperty factory to handle data types
+        """
+        attribute = validated_data.get("attribute")
+        factory = ResultPropertyFactory(attribute.data_type)
+
+        return factory.make(
+            result=self.result,
+            attribute=attribute,
+            value=validated_data.get("value")
+        )
+
