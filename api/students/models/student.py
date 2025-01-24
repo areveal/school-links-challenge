4d2f13b39f1720f3733067c7b77a672cc2f096ee
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-23 20:50:55 -0600

    Adds Student string method

--- api/students/models/student.py
+++ api/students/models/student.py
@@ -5,3 +5,6 @@ class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, null=True, default=None)
     district = models.ForeignKey('districts.District', related_name="students", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
