d907e40601fa755f0efce5782b553c7865f568af
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-26 14:43:10 -0600

    Fix active css styling for NavBar
    Ensure NavBar elements show as active when on the correct path

--- frontend/src/components/SchoolNavBar.jsx
+++ frontend/src/components/SchoolNavBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import {Nav, Navbar, NavbarBrand} from "react-bootstrap";
+import {NavLink} from "react-router";
 
 function SchoolNavBar() {
     return (
         <Navbar>
             <NavbarBrand>School Links</NavbarBrand>
             <Nav>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/students">Students</Nav.Link>
-                <Nav.Link href="/exams">Exams</Nav.Link>
-                <Nav.Link href="/exam-results">Exam Results</Nav.Link>
+                <Nav.Link as={NavLink} to="/" exact activeClassName="active">Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/students" activeClassName="active">Students</Nav.Link>
+                <Nav.Link as={NavLink} to="/exams" activeClassName="active">Exams</Nav.Link>
+                <Nav.Link as={NavLink} to="/exam-results" activeClassName="active">Exam Results</Nav.Link>
             </Nav>
         </Navbar>
     );
