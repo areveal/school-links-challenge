24b05f0a25ddbda25caa7b08f5f36bfc6d2d343c
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-26 12:12:17 -0600

    Adds routes to App
    Adds different routes to application for handling expected paths for frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./pages/Home";
+import Students from "./pages/Students";
+import Exams from "./pages/Exams";
+import ExamResults from "./pages/ExamResults";
+import StudentDetails from "./pages/StudentDetails";
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} name="home" />
+            <Route path="/students" element={<Students />} name="students" />
+            <Route path="/students/:id" element={<StudentDetails />} name="student-details" />
+            <Route path="/exams" element={<Exams />} name="exams" />
+            <Route path="/exam-results" element={<ExamResults />} name="exam-results" />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
