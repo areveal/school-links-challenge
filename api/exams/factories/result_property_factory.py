10f2086c26571c1ababec309b42a6d6f980d4f89
Alexi Cole Reveal <alexi.reveal@gmail.com>
2025-01-29 11:49:29 -0600

    Modifies ResultPropertyFactory
    Simplifies ResultPropertyFactory and improves performance.
    
    Eliminates the need to instantiate a new factory for each new property being created.
    
    Also prevents unnecessary error of setting data type as something other than the ExamAttribute data type.

--- api/exams/factories/result_property_factory.py
+++ api/exams/factories/result_property_factory.py
@@ -11,16 +11,10 @@ from exams.models import (
 
 
 class ResultPropertyFactory:
-    def __init__(self, data_type: ExamAttribute.DataTypeChoices):
-        self.data_type = data_type
-
-    def derive_result_property_model_class(self) -> type[ResultProperty]:
+    @staticmethod
+    def derive_result_property_model_class(data_type) -> type[ResultProperty]:
         """
         Derives the appropriate result property model class based on the data type.
-
-        Returns:
-        type[ResultProperty]
-            The model class corresponding to the specified data type.
         """
         return {
             "str": StringResultProperty,
@@ -29,7 +23,16 @@ class ResultPropertyFactory:
             "bool": BooleanResultProperty,
             "date": DateResultProperty,
             "datetime": DateTimeResultProperty,
-        }.get(self.data_type)
+        }.get(data_type)
 
-    def make(self, **kwargs) -> ResultProperty:
-        return self.derive_result_property_model_class().objects.create(**kwargs)
+    @staticmethod
+    def make(**kwargs) -> ResultProperty:
+        """
+        Factory make method that derives correct ResultProperty inherited model from the given ExamAttribute
+        and then calls create with provided kwargs
+        """
+        exam_attribute = kwargs.get("attribute")
+        data_type = exam_attribute.data_type
+        result_property_model = ResultPropertyFactory.derive_result_property_model_class(data_type=data_type)
+
+        return result_property_model.objects.create(**kwargs)
